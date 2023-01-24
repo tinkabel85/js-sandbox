a7a886e898ca0be37bb160e0c2c449e1442ca683
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-01-24 10:26:04 +0100

    added math calculations

---
+++ variable.js
@@ -0,0 +1,24 @@
+let number = 7;
+let a = 8;
+let b = 5;
+
+let c = a * b;
+a = a + 1; 
+
+let e = 14;
+console.log("E is :", e);
+e = e + 1; // e++;
+e += 2; 
+console.log("E updated is :", e);
+e = e - 1; // e--;
+console.log(number);
+console.log(c);
+
+
+let even = 44;
+let odd = 57;
+
+console.log(even % 2);
+console.log(odd % 2);
+
+
