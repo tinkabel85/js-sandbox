6d3dae20bae55a1e015f3383146ca5a2495d5689
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-01-25 13:18:44 +0100

    corrected  task 5 Control Structures

--- controlStructures/task5.js
+++ controlStructures/task5.js
@@ -1,18 +1,21 @@
 // Task5:
 // Write a program, that counts how often each character is included in a string
 
-let myString = "mamama";
+let myString = "geeksforgeeks";
 
-for (let i = 0; i < myString.length; i++) {
-    let count = 1;
-    for (let j = i + 1; j < myString.length; j++) {
-        if (myString[i] === myString[j]) {
-            count++;
-        }
+for (let i = 0; i <= myString.length ; i++) {
+	let count = 0;
+	for (let j = 0; j <= myString.length; j++) {
+		if (myString[i] === myString[j]) {
+			count++;
+			//console.log(myString[i] + " " + count);
+		}
     }
-	if (count > 1) {
+    if (count > 1) {
 		console.log(`${myString[i]} appears ${count} time(s)`);
-	}
+        myString = myString.replaceAll(myString[i], "");
+        console.log(myString);
+		}
 }
 
 console.log("****************************************************************");
