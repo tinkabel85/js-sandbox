60ff717b9279bb6bb764ca42ee0d5a11fbd2a97d
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-01-25 14:06:34 +0100

    added task2 ControlStructuresHard

--- controlStructures/task5.js
+++ controlStructures/task5.js
@@ -3,19 +3,19 @@
 
 let myString = "geeksforgeeks";
 
-for (let i = 0; i <= myString.length ; i++) {
+for (let i = 0; i <= myString.length; i++) {
 	let count = 0;
 	for (let j = 0; j <= myString.length; j++) {
 		if (myString[i] === myString[j]) {
 			count++;
 			//console.log(myString[i] + " " + count);
 		}
-    }
-    if (count > 1) {
+	}
+	if (count > 1) {
 		console.log(`${myString[i]} appears ${count} time(s)`);
-        myString = myString.replaceAll(myString[i], "");
-        console.log(myString);
-		}
+		myString = myString.replaceAll(myString[i], "");
+		console.log(myString);
+	}
 }
 
 console.log("****************************************************************");
