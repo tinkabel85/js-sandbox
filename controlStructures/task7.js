802a1c3e1c6d88a4433328bf24d767d28d2d5287
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-01-25 12:11:26 +0100

    added task7

---
+++ controlStructures/task7.js
@@ -0,0 +1,9 @@
+// Task7:
+// Write a program, that concatinates a given string n times
+let n = 7;
+let string = "Hello";
+let result = "";
+for (let i = 0; i < n; i++) {
+    result = result + string;
+}
+console.log(result);
