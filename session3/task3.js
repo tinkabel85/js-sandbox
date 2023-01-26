e392991fcac5b47fc6c1938920e1f4930d9ac213
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-01-26 16:03:38 +0100

    added task 3

---
+++ session3/task3.js
@@ -0,0 +1,10 @@
+// Task 3
+// Create a function that takes in a number and returns the result of raising it to the power of 3.
+
+let number = 3;
+
+function powerOfThree(n) {
+    return n ** 3;
+}
+
+console.log(`The number ${number} to power of three is ${powerOfThree(number)}`);
