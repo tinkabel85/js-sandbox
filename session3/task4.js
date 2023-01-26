9296945c2620f27dad6ac20c8528e9854c39d977
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-01-26 16:04:03 +0100

    added task 4

---
+++ session3/task4.js
@@ -0,0 +1,11 @@
+// Task 4
+// Create a function that takes in a number as an argument and returns true if it is even and false if it is odd.
+
+let num = 3;
+
+function isEven(n) {
+    return n % 2 == 0;
+}
+
+
+console.log(`The answer is ${isEven(num)}.`);
