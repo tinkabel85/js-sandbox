000b2b249fb2144642db79025148835eec219348
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-01-26 16:05:48 +0100

    added task 12

---
+++ session3/task12.js
@@ -0,0 +1,24 @@
+// Create a function that takes in a number as an argument 
+//and returns the factorial of that number using recursion.
+
+let n = 5;
+
+function factorial(n) {
+    if (n <= 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(n));
+
+// factorial(5) =>
+//              return 5 * factorial(4)
+//                         => return 4 * factorial(3)
+//                                       => return 3 * factorial(2)
+//                                                     => return 2 * factorial(1)
+//                                                                   => return 1
+//                                                        return 2 * 1
+//                                         return 3 *2 * 1
+//                            return 4 * 3 * 2 * 1
+//            return 5 * 4 * 3 * 2 * 1
