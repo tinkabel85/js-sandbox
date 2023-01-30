cca0d7c72825bb6cb3c6a5fab98355b778e44483
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-01-30 23:41:02 +0100

    added updated version of hamburger menu

--- session5/hamburgerMenu/script.js
+++ session5/hamburgerMenu/script.js
@@ -1,6 +1,6 @@
-const hamburger= document.querySelector(".hamburger");
+const hamburger = document.querySelector(".checkbox ");
 const menu = document.querySelector(".menu");
-
+console.log('hello')
 hamburger.addEventListener("click", (e) => {
 	menu.classList.toggle("clicked");
 });
